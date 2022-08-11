c8395e278b5d5ae2661d70e8f06e760640422354
efurst <efurst1@depaul.edu>
2022-08-11 09:31:06 -0500

    remove accumulate

--- loss.py
+++ loss.py
@@ -1,6 +1,5 @@
 import torch
 
-
 class ERMLoss:
     """
     Implements a standard Empirical Risk Minimization loss function
@@ -8,26 +7,16 @@ class ERMLoss:
     """
 
     def __init__(self, model, loss_fn):
-        self.accumulated = torch.tensor(0.0)
         self.model = model
         self.loss_fn = loss_fn
 
-    def compute_loss(self, preds, y, accumulate=False):
-        if self.accumulated.device != y.device:
-            self.accumulated = self.accumulated.to(y.device)
-        self.accumulated += self.loss_fn(preds, y)
+    def compute_loss(self, preds, y):
+        return self.loss_fn(preds, y)
 
-        loss = self.accumulated
-
-        if not accumulate:
-            self.accumulated = torch.tensor(0.0)
-
-        return loss
-
-    def __call__(self, minibatch, accumulate=False):
+    def __call__(self, minibatch):
         X, y, _ = minibatch
 
-        return self.compute_loss(self.model(X), y, accumulate)
+        return self.compute_loss(self.model(X), y)
 
 
 class GDROLoss:
@@ -37,7 +26,6 @@ class GDROLoss:
     """
 
     def __init__(self, model, loss_fn, eta, num_subclasses, vector_subclass=False):
-        self.accumulated = [torch.zeros(num_subclasses), torch.zeros(num_subclasses)]
         self.model = model
         self.loss_fn = loss_fn
         self.q = torch.ones(num_subclasses) / num_subclasses
@@ -45,16 +33,14 @@ class GDROLoss:
         self.vector_subclass = vector_subclass
         self.num_subclasses = num_subclasses
 
-    def compute_loss(self, preds, y, c, accumulate=False):
+    def compute_loss(self, preds, y, c):
         device = y.device
-        self.accumulated[0] = self.accumulated[0].to(device)
-        self.accumulated[1] = self.accumulated[1].to(device)
 
         if self.q.device != device:
             self.q = self.q.to(device)
 
-        losses = self.accumulated[0]
-        subclass_counts = self.accumulated[1]
+        losses = torch.zeros(self.num_subclasses)
+        subclass_counts = torch.zeros(self.num_subclasses)
 
         for subclass in range(self.num_subclasses):
 
@@ -66,27 +52,22 @@ class GDROLoss:
             if torch.sum(subclass_idx) > 0:
                 losses[subclass] += self.loss_fn(preds[subclass_idx], y[subclass_idx])
 
-        self.accumulated = [losses, subclass_counts]
-
         # update q
-        if self.model.training and not accumulate:
+        if self.model.training:
             self.q *= torch.exp(self.eta * losses.data)
             self.q /= self.q.sum()
 
         loss = torch.dot(losses, self.q)
 
-        if not accumulate:
-            self.accumulated = [torch.zeros(self.num_subclasses), torch.zeros(self.num_subclasses)]
-
         return loss
 
-    def __call__(self, minibatch, accumulate=False):
+    def __call__(self, minibatch):
         
         X, y, c = minibatch
 
         preds = self.model(X)
 
-        return self.compute_loss(preds, y, c, accumulate)
+        return self.compute_loss(preds, y, c)
 
 
 class ERMGDROLoss:
@@ -101,14 +82,13 @@ class ERMGDROLoss:
         self.model = model
         self.t = t
 
-    def compute_loss(self, preds, y, c, accumulate=False):
-        return self.t * self.erm.compute_loss(preds, y, accumulate) + \
-               (1 - self.t) * self.gdro.compute_loss(preds, y, c, accumulate)
+    def compute_loss(self, preds, y, c):
+        return self.t * self.erm.compute_loss(preds, y) + (1 - self.t) * self.gdro.compute_loss(preds, y, c)
 
-    def __call__(self, minibatch, accumulate=False):
+    def __call__(self, minibatch):
         X, y, c = minibatch
         preds = self.model(X)
-        return self.compute_loss(preds, y, c, accumulate)
+        return self.compute_loss(preds, y, c)
 
 
 class UpweightLoss:
@@ -121,8 +101,8 @@ class UpweightLoss:
         # wraps a GDROLoss with eta=0
         self.gdro = GDROLoss(model, loss_fn, eta=0, num_subclasses=num_subclasses, vector_subclass=vector_subclass)
 
-    def compute_loss(self, preds, y, c, accumulate=False):
-        return self.gdro.compute_loss(preds, y, c, accumulate)
+    def compute_loss(self, preds, y, c):
+        return self.gdro.compute_loss(preds, y, c)
 
-    def __call__(self, minibatch, accumulate=False):
-        return self.gdro(minibatch, accumulate)
+    def __call__(self, minibatch):
+        return self.gdro(minibatch)
