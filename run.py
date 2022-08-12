1017da7d0139f3dc85933555d16fa58e73a38f25
efurst <efurst1@depaul.edu>
2022-08-12 12:19:26 -0500

    mnist model uses correct device

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ elif args.dataset == 'mnist':
 
     run_trials_args = {
         'model_class': models.NeuralNetwork,
-        'model_args': {'layers': [28 * 28, 256, 64, 10]},
+        'model_args': {'layers': [28 * 28, 256, 64, 10], 'device': device},
         'epochs': 10,
         'optimizer_class': torch.optim.Adam,
         'optimizer_args': {'lr': 0.0005, 'weight_decay': 0.005},
