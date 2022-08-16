344d67be947edc13735ccd3c32399119910b97ad
Thomas Zeng <tomzeng200@gmail.com>
2022-08-16 13:14:08 -0500

    adds ability to get sc scores

--- extract_WB_features.py
+++ extract_WB_features.py
@@ -1,10 +1,17 @@
 import pandas as pd
-from utils.process_data_utils import  get_dataloaders, get_waterbirds_datasets, split_dataset, get_partitioned_dataloader
-from loss import ERMLoss, GDROLoss, ERMGDROLoss
-from train_eval import train, evaluate, train_epochs
+from utils.process_data_utils import  get_dataloaders, get_waterbirds_datasets
+from loss import ERMLoss, GDROLoss
+from train_eval import  train_epochs
 import torch
 from models import TransferModel50
 import argparse
+
+import pandas as pd
+import numpy as np
+
+from umap import UMAP
+from matplotlib import pyplot as plt
+from sklearn.metrics import silhouette_score
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file_name', default='features')
@@ -13,6 +20,7 @@ parser.add_argument('-v', '--verbose', action='store_true')
 parser.add_argument('-r', '--reweight_train', action='store_true')
 parser.add_argument('-s', '--subclass_label', action='store_true')
 parser.add_argument('-l', '--loss_function', default='ERM')
+parser.add_argument('-sc', '--silhouette_score', action='store_true')
 
 
 args = parser.parse_args()
@@ -30,61 +38,72 @@ batch_size = (128, 128)
 eta = 0.01
 num_subclasses = 4
 num_labels = 4 if args.subclass_label else 2
+epochs = 131
+
+#For GDRO, we must reweight
+if args.loss_function == 'GDRO':
+    args.reweight_train =True
 
 train_dataset, val_dataset, test_dataset = get_waterbirds_datasets(device=device, subclass_label=args.subclass_label)
 train_dataloader, val_dataloader, test_dataloader = get_dataloaders((train_dataset, val_dataset, test_dataset), batch_size=batch_size, reweight_train=args.reweight_train)
 
 
-epochs = 131
+trials = 5 if args.silhouette_score else 1
 
-model = TransferModel50(device=device, num_labels=num_labels)
-if args.loss_function == 'ERM':
-    loss_fn = ERMLoss(model, torch.nn.CrossEntropyLoss())
-else:
-    loss_fn = GDROLoss(model, torch.nn.CrossEntropyLoss(), eta=eta, num_subclasses=num_subclasses)
+for _ in range(trials):
+
+    model = TransferModel50(device=device, num_labels=num_labels)
 
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, weight_decay=0.0001, momentum=0.9)
+    if args.loss_function == 'ERM':
+        loss_fn = ERMLoss(model, torch.nn.CrossEntropyLoss())
+    else:
+        loss_fn = GDROLoss(model, torch.nn.CrossEntropyLoss(), eta=eta, num_subclasses=num_subclasses)
 
-train_epochs(epochs, train_dataloader, val_dataloader, test_dataloader, model, loss_fn, optimizer, verbose=args.verbose, num_subclasses=num_subclasses)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, weight_decay=0.0001, momentum=0.9)
 
-activation = {}
-def get_activation(name):
-    def hook(model, input, output):
-        activation[name] = output.detach()
-    return hook
+    train_epochs(epochs, train_dataloader, val_dataloader, test_dataloader, model, loss_fn, optimizer, verbose=args.verbose, num_subclasses=num_subclasses)
 
-model.model.avgpool.register_forward_hook(get_activation('avgpool'))
+    activation = {}
+    def get_activation(name):
+        def hook(model, input, output):
+            activation[name] = output.detach()
+        return hook
 
-model.eval()
+    model.model.avgpool.register_forward_hook(get_activation('avgpool'))
 
-features = []
-labels = []
+    model.eval()
 
-for _ in range(test_dataloader.batches_per_epoch()):
-    X,y,c = next(test_dataloader)
+    features = []
+    labels = []
 
-    model(X)
-    img_features = activation['avgpool'].squeeze()
-    features.extend(torch.unbind(img_features))
+    for _ in range(test_dataloader.batches_per_epoch()):
+        X,y,c = next(test_dataloader)
 
-    labels.extend(c.int().tolist())
+        model(X)
+        img_features = activation['avgpool'].squeeze()
+        features.extend(torch.unbind(img_features))
+
+        labels.extend(c.int().tolist())
 
 
-for _ in range(val_dataloader.batches_per_epoch()):
-    X,y,c = next(val_dataloader)
+    for _ in range(val_dataloader.batches_per_epoch()):
+        X,y,c = next(val_dataloader)
 
-    model(X)
-    img_features = activation['avgpool'].squeeze()
-    features.extend(torch.unbind(img_features))
+        model(X)
+        img_features = activation['avgpool'].squeeze()
+        features.extend(torch.unbind(img_features))
 
-    labels.extend(c.int().tolist())
+        labels.extend(c.int().tolist())
 
-cols = []
-for idx,label in enumerate(labels):
-    cols.append([label] + features[idx].cpu().numpy().tolist())
+    cols = []
+    for idx,label in enumerate(labels):
+        cols.append([label] + features[idx].cpu().numpy().tolist())
 
-df_feats = pd.DataFrame(cols).rename({0:'label'}, axis=1)
+    df_feats = pd.DataFrame(cols).rename({0:'label'}, axis=1)
+
+    if args.silhouette_score:
+
 
 df_feats.to_csv(f'{args.file_name}.csv')
 
