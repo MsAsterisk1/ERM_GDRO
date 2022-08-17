1740bc4b84101cc5d87453201279d6da7a4c4332
efurst <efurst1@depaul.edu>
2022-08-17 16:43:14 -0500

    formatting and documentation

--- run.py
+++ run.py
@@ -10,15 +10,15 @@ from math import ceil
 import transformers
 
 parser = argparse.ArgumentParser()
-parser.add_argument('dataset')
-parser.add_argument('loss')
-parser.add_argument('--trials', default=5)
-parser.add_argument('--test_name', default='test')
-parser.add_argument('--device', default="0" if torch.cuda.is_available() else "cpu")
-parser.add_argument('--verbose', action='store_true')
-parser.add_argument('--cris_prop', default=0.7)
-parser.add_argument('--multiclass', action='store_true')
-parser.add_argument('--val', default=None)
+parser.add_argument('dataset', help='The dataset to use ex. waterbirds, civilcomments, celeba')
+parser.add_argument('loss', help='The loss scheme to use ex. erm, gdro, cris')
+parser.add_argument('--trials', default=5, help='The number of trials to run, default is 5')
+parser.add_argument('--test_name', default='test', help='The name of the test to run, the file will be saved at ./test_results/[name].csv')
+parser.add_argument('--device', default="0" if torch.cuda.is_available() else "cpu", help='The GPU to use. If left blank, will default to GPU 0 or CPU if cuda is unavailable')
+parser.add_argument('--verbose', action='store_true', help='Whether to print the progress and epoch results')
+parser.add_argument('--cris_prop', default=0.7, help='The proportion of data to be dedicated to training the featurizer when using CRIS (ignored if not using CRIS)')
+parser.add_argument('--subclass_labels', action='store_true', help='Trains the model using the subclass labels (evalutaion is still done using the superclass labels, but is done with respect to each subclass)')
+parser.add_argument('--val', default=None, help='If equal to 0, best overall validation accuracy is tracked and printed at the end of each trial. If another integer, best worst-group accuracy is used instead. If left empty, validation accuracy is not tracked')
 
 
 args = parser.parse_args()
@@ -33,7 +33,7 @@ else:
 
 if args.dataset == 'waterbirds':
 
-    train_dataset, val_dataset, test_dataset = utils.get_waterbirds_datasets(device=device, subclass_label=args.multiclass)
+    train_dataset, val_dataset, test_dataset = utils.get_waterbirds_datasets(device=device, subclass_label=args.subclass_labels)
 
     # From Distributionally Robust Neural Networks
     batch_size = (128, 512)
@@ -42,28 +42,28 @@ if args.dataset == 'waterbirds':
 
     run_trials_args = {
         'model_class': models.TransferModel50,
-        'model_args': {'device': device, 'freeze': False, 'num_labels': 4 if args.multiclass else 2},
+        'model_args': {'device': device, 'freeze': False, 'num_labels': 4 if args.subclass_labels else 2},
         'epochs': 300,
         'optimizer_class': torch.optim.SGD,
         'optimizer_args': {'lr': 0.0001, 'weight_decay': 0.0001},  # , 'momentum': 0.9},
         'num_subclasses': 4,
     }
 elif args.dataset == 'mnist':
-    train_dataset, val_dataset, test_dataset = utils.get_MNIST_datasets(device=device, subclass_label=args.multiclass)
+    train_dataset, val_dataset, test_dataset = utils.get_MNIST_datasets(device=device, subclass_label=args.subclass_labels)
     batch_size = (1024, 1024)
     eta = 0.01
     num_subclasses = 10
 
     run_trials_args = {
         'model_class': models.NeuralNetwork,
-        'model_args': {'layers': [28 * 28, 256, 64, 10 if args.multiclass else 2], 'device': device},
+        'model_args': {'layers': [28 * 28, 256, 64, 10 if args.subclass_labels else 2], 'device': device},
         'epochs': 10,
         'optimizer_class': torch.optim.Adam,
         'optimizer_args': {'lr': 0.0005, 'weight_decay': 0.005},
         'num_subclasses': 10,
     }
 elif args.dataset == 'civilcomments':
-    train_dataset, val_dataset, test_dataset = utils.get_CivilComments_Datasets(device=device, subclass_label=args.multiclass)
+    train_dataset, val_dataset, test_dataset = utils.get_CivilComments_Datasets(device=device, subclass_label=args.subclass_labels)
     batch_size = (16, 128)
 
     # for gdro only train on labels as classes
@@ -76,7 +76,7 @@ elif args.dataset == 'civilcomments':
 
     run_trials_args = {
         'model_class': models.BertClassifier,
-        'model_args': {'device': device, 'num_labels': 4 if args.multiclass else 2},
+        'model_args': {'device': device, 'num_labels': 4 if args.subclass_labels else 2},
         'epochs': 5,
         'optimizer_class': torch.optim.AdamW,
         'optimizer_args': {'lr': 0.00001, 'weight_decay': 0.01},
@@ -87,14 +87,14 @@ elif args.dataset == 'civilcomments':
         'vector_subclass': True
     }
 elif args.dataset == 'celeba':
-    train_dataset, val_dataset, test_dataset = utils.get_celeba_datasets(device=device, subclass_label=args.multiclass)
+    train_dataset, val_dataset, test_dataset = utils.get_celeba_datasets(device=device, subclass_label=args.subclass_labels)
     batch_size = (128, 128)
 
     num_subclasses = 4
 
     run_trials_args = {
         'model_class': models.TransferModel50,
-        'model_args': {'device': device, 'freeze': False, 'num_labels': 4 if args.multiclass else 2},
+        'model_args': {'device': device, 'freeze': False, 'num_labels': 4 if args.subclass_labels else 2},
         'epochs': 50,
         'optimizer_class': torch.optim.SGD,
         'optimizer_args': {'lr': 0.0001, 'weight_decay': 0.0001, 'momentum': 0.9},
@@ -109,7 +109,7 @@ run_trials_args['verbose'] = args.verbose
 run_trials_args['record'] = True
 run_trials_args['validation'] = int(args.val)
 
-run_trials_args['multiclass'] = args.multiclass
+run_trials_args['subclass_labels'] = args.subclass_labels
 
 results_dir = 'test_results/'
 test_name = args.test_name
