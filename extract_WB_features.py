382e982ca26816afe1e30ae02edb9a81f1b84bec
Thomas Zeng <tzeng@aiscalar.cdm.depaul.edu>
2022-08-17 09:16:57 -0500

    fixes missing code

--- extract_WB_features.py
+++ extract_WB_features.py
@@ -100,7 +100,7 @@ for trial in tqdm(range(trials)):
     for _ in range(val_dataloader.batches_per_epoch()):
         X,y,c = next(val_dataloader)
 
-        model(X)
+        preds = model(X)
         img_features = activation['avgpool'].squeeze()
         features.extend(torch.unbind(img_features))
         pred_labels.extend(preds.int().tolist())
